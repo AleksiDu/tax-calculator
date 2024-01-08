60fc3cfda5f081477ed055ffce9c580ae703d79a
AleksiDu <aleksi.duluzauri@gmail.com>
2024-01-08 11:08:46 +0400

    add logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use std::string;
+
 slint::include_modules!();
+
+
+const TAXPER: f64 = 0.20;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPERXPER: f64 = 0.20;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
+    ui.on_divide_income(move | string | {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        let num:f64 = string.trim().parse().unwrap();
+        let tax: f64 = num * TAXPER;
+        let owner: f64 = num * OWNERPER;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPERXPER;
+        let result = format!("Taxes: {:2}\nOwner {:2}\nProfit: {:2}\nOpEx {:2}", {tax}, {owner}, {profit}, {opex});
+        ui.set_results(result.into());
     });
 
     ui.run()
